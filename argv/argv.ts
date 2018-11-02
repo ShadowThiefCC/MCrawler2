4745afe976600726bbbb7848fe2e9ff4b7c9df79
HaoDaWang <1306036576@qq.com>
2018-11-02 11:36:32 +0800

    Remove parse

--- argv/argv.ts
+++ argv/argv.ts
@@ -1,6 +1,8 @@
-export let isEmpty = function(flag:string){
-    if(!flag) {
-        console.log("usage: 学号 操作符 练习题号")
-        process.exit()
-    }
-}
+import { program } from "../commander";
+
+export let isEmpty = function(flag: string) {
+  if (!flag) {
+    program.outputHelp();
+    process.exit();
+  }
+};
