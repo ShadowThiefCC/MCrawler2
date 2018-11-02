9c0afe8a38b8b3cfb6fc90b70e821f0558e49256
HaoDaWang <1306036576@qq.com>
2018-11-02 11:36:38 +0800

    Add parse

--- commander/commander.ts
+++ commander/commander.ts
@@ -2,10 +2,10 @@ import * as program from "commander";
 
 program
   .version("1.0.0")
-  .option("-s, --studentID", "Add student ID")
-  .option("-t, --type", "Type of operate")
-  .option("-c, --testID", "Add test ID")
-  .option("-t, --teacher", "The teacher that teaches you")
+  .option("-s, --studentID [value]", "Add student ID")
+  .option("-t, --type [value]", "Type of operate")
+  .option("-c, --testID [value]", "Add test ID")
+  .option("-l, --teacher [value]", "The teacher that teaches you")
   .parse(process.argv);
 
 export { program };
